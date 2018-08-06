29b3d9d2b9e5cc862dcc960dc1ad81ae9bdd90f6
optimistguy <41352253+optimistguy@users.noreply.github.com>
2018-08-06 19:53:50 +0700

    Update bot.py
    added some updates

--- bot.py
+++ bot.py
@@ -1,12 +1,38 @@
 import discord
 from discord.ext import commands
 
-TOKEN = ''
+TOKEN = 'NDc1OTcxNzYwMjkxMDUzNTY5.Dkm_lw.MZr8uiHf4Pl6LoLQy8YiUfyAQKo'
 
-client = commands.Bot(command_prefix = './CST')
+client = commands.Bot(command_prefix = '.', case_insensitive=False)
+client.remove_command('help')
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name='SomethingElse'))
     print('Bot is ready.')
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='Servants 🌾')
+    await client.add.roles(member, role)
+    await client.say('Hi ' + member + '. ' + 'Wellcome to this community')
+
+
+@client.command()
+async def ping():
+    await client.say('Pong!')
+
+@client.command()
+async def pong():
+    await client.say('There are no game called Pong Ping in my library :(')
+
+@client.command()
+async def help():
+    await client.say('Sending some help')
+    sleep(2)
+    await client.say('Wait')
+    sleep(1)
+    await client.say('@Josh ¯\_(ツ)_/¯#1102 still working on it')
+
 
 client.run(TOKEN)
