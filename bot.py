eeb790edd630642c3313403f40ff36636bc60448
optimistguy <41352253+optimistguy@users.noreply.github.com>
2018-08-06 17:29:21 +0700

    Add files via upload
    the main bot file

---
+++ bot.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+TOKEN = ''
+
+client = commands.Bot(command_prefix = './CST')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+client.run(TOKEN)
