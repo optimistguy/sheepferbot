2718d759412aa4f49dc3f119292d192a9f51ec05
optimistguy <41352253+optimistguy@users.noreply.github.com>
2018-08-06 20:58:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import discord
+import asyncio
 from discord.ext import commands
+from discord.ext.commands import Bot
 
-TOKEN = 'SomethingElse'
+TOKEN = 'NDc1OTcxNzYwMjkxMDUzNTY5.Dkm_lw.MZr8uiHf4Pl6LoLQy8YiUfyAQKo'
 
-client = commands.Bot(command_prefix = '.', case_insensitive=False)
+client = commands.Bot(command_prefix = 's!', case_insensitive=False)
 client.remove_command('help')
 
 @client.event
@@ -17,6 +19,25 @@ async def on_member_join(member):
     await client.add.roles(member, role)
     await client.say('Hi ' + member + '. ' + 'Wellcome to this community')
 
+@client.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('.hack'):
+        msg = 'I am just a sheepfer {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
 @client.command()
 async def ping():
@@ -29,10 +50,20 @@ async def pong():
 @client.command()
 async def help():
     await client.say('Sending some help')
-    sleep(2)
     await client.say('Wait')
-    sleep(1)
     await client.say('@Josh ¯\_(ツ)_/¯#1102 still working on it')
+
+@client.command()
+async def hack():
+    msg = 'I am just a sheepfer {0.author.mention}'.format(msg)
+    await client.send_message(message.channel, msg)
+
+@client.command()
+async def img():
+    msg = 'I am just a sheepfer {0.author.mention}'.format(msg)
+    await client.send_message(message.channel, msg)
+
+
 
 
 client.run(TOKEN)
